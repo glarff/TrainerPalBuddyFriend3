9245748f60fa062ed1efa468b89fc9465839223b
BHAGWAN0 1 <michael.ross444@gmail.com>
2019-07-29 13:36:36 -0400

    various misc changes

--- TrainerPalBuddyFriend3/Controllers/HomeController.cs
+++ TrainerPalBuddyFriend3/Controllers/HomeController.cs
@@ -573,20 +573,157 @@ namespace TrainerPalBuddyFriend3.Controllers
         {
             if (ModelState.IsValid)
             {
-                // Get data for existing Wksegs
                 using (ISession _S = MvcApplication.SF.GetCurrentSession())
                 {
+                    // Get segment list
+                    Segments sg = null;
+
+                    var sgList = _S.QueryOver<Segments>(() => sg)
+                    .SelectList(l => l
+                        .Select(x => x.Segmentpk).WithAlias(() => sg.Segmentpk)
+                        .Select(x => x.Name).WithAlias(() => sg.Name)
+                    )
+                    .TransformUsing(Transformers.AliasToBean<Segments>())
+                    .List<Segments>();
+
+                    var list2 = new List<MyListTable>();
+
+                    foreach (var r in sgList)
+                    {
+                        list2.Add(new MyListTable
+                        {
+                            Key = r.Segmentpk,
+                            Display = r.Name.ToString()
+                        });
+                    }
+
+                    // Get data for existing Wksegs
+
                     Wkseg wks = null;
 
-                    var vksList = _S.QueryOver<Wkseg>(() => wks)
+                    var wksList = _S.QueryOver<Wkseg>(() => wks)
                         .SelectList(l => l
-                            .Select(x => x.Wksegpk))
+                            .Select(x => x.Wksegpk).WithAlias(() => wks.Wksegpk)
+                            .Select(x => x.Workouts).WithAlias(() => wks.Workouts)
+                            .Select(x => x.Segments).WithAlias(() => wks.Segments)
+                            .Select(x => x.Duration).WithAlias(() => wks.Duration)
+                            .Select(x => x.Sequence).WithAlias(() => wks.Sequence)
+                            )
+                        .Where(x => x.Workouts == d.SelectedWorkout)
+                        .TransformUsing(Transformers.AliasToBean<Wkseg>())
+                        .List<Wkseg>();
+
+                    d.Dts = new List<Raszagal>();
+
+                    foreach(var z in wksList)
+                    {
+                        var rWksg = new Wkseg
+                        {
+                            Wksegpk = z.Wksegpk,
+                            Workouts = z.Workouts,
+                            Segments = z.Segments,
+                            Duration = z.Duration,
+                            Sequence = z.Sequence,
+                            DropDownList2 = new SelectList(list2, "Key", "Display")
+                        };
+
+                        var rz = new Raszagal();
+                        rz.rWkseg = rWksg;
+
+                        d.Dts.Add(rz);
+                    }
                 }
 
 
                     return View("WkSeg", d);
             }
 
+            else
+            {
+                return View("Index");
+            }
+        }
+
+        public ActionResult Form8(Danimoth d)
+        {
+            if (ModelState.IsValid)
+            {
+                // Get existing types
+                var newListPKs = new List<int>();
+                var oldListPKs = new List<int>();
+
+                using (ISession _S = MvcApplication.SF.GetCurrentSession())
+                {
+                    Wkseg tp = null;
+
+                    var oldList = _S.QueryOver<Wkseg>(() => tp)
+                        .SelectList(l => l
+                            .Select(x => x.Wksegpk).WithAlias(() => tp.Wksegpk)
+                        )
+                        .TransformUsing(Transformers.AliasToBean<Wkseg>())
+                        .List<Wkseg>();
+
+                    foreach (var r in oldList)
+                    {
+                        oldListPKs.Add(r.Wksegpk);
+                    }
+
+                    foreach (var r in d.Dts)
+                    {
+                        newListPKs.Add(r.rWkseg.Wksegpk);
+                    }
+
+                    foreach (var q in d.Dts)
+                    {
+                        //updates
+                        if (oldListPKs.Contains(q.rWkseg.Wksegpk))
+                        {
+                            var persistentType = _S.Load<Wkseg>(q.rWkseg.Wksegpk);
+
+                            persistentType.Workouts = q.rWkseg.Workouts;
+                            persistentType.Segments = q.rWkseg.Segments;
+                            persistentType.Duration = q.rWkseg.Duration;
+                            persistentType.Sequence = q.rWkseg.Sequence;
+
+                            _S.Save(persistentType);
+                            _S.Flush();
+                            _S.Clear();
+                        }
+
+                        //inserts
+                        else if (q.rWkseg.Wksegpk == -1)
+                        {
+                            var persistentType = new Wkseg();
+
+                            persistentType.Workouts = q.rWkseg.Workouts;
+                            persistentType.Segments = q.rWkseg.Segments;
+                            persistentType.Duration = q.rWkseg.Duration;
+                            persistentType.Sequence = q.rWkseg.Sequence;
+
+                            _S.Save(persistentType);
+                            _S.Flush();
+                            _S.Clear();
+                        }
+                    }
+
+                    //deletions
+                    foreach (var x in oldListPKs)
+                    {
+                        if (!(newListPKs.Contains(x)))
+                        {
+                            var persistentType = _S.Load<Wkseg>(x);
+
+                            _S.Delete(persistentType);
+                            _S.Flush();
+                            _S.Clear();
+                        }
+                    }
+                }
+
+                ViewBag.onSuccess_Message = "WkSeg updated successfully.";
+
+                return View("Index");
+            }
             else
             {
                 return View("Index");
